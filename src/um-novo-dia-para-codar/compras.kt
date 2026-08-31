33d05b9865f38f5cb08cd8b9b659b1a4602768ce
zerowavez <lucastorinopam@gmail.com>
2026-08-31 12:33:51 -0300

    compras feito

--- src/um-novo-dia-para-codar/compras.kt
+++ src/um-novo-dia-para-codar/compras.kt
@@ -5,3 +5,27 @@
 //Sempre que o usuário procurar por uma fruta que não está no array exiba a mensagem "Fruta indisponível no nosso mercado".
 //Quando o usuário digitar "PARE", exiba a lista de frutas restantes.
 //Quando o array não possuir mais itens dentro de si, escreva "Lista de compras finalizada".
+
+fun main() {
+    var continuar = true
+    val frutas = mutableListOf<String>("Maçã", "Banana", "Pitaia", "Graviola", "Carambola", "Lichia")
+
+    println("Essa é sua lista de compras!\n- ${frutas.joinToString("\n- ")}")
+
+    while (continuar) {
+        var fruta = readString("Digite o nome de uma fruta: ", 1).lowercase().replaceFirstChar { it.uppercase() }
+
+        if (fruta in frutas) {
+            println("$fruta foi retirada da lista!")
+            frutas.remove(fruta)
+        } else {
+            println("Fruta indisponível em nosso mercado!")
+        }
+
+        if (frutas.isEmpty()) {
+            continuar = false
+        }
+    }
+
+    println("Lista de compras finalizada!")
+}
